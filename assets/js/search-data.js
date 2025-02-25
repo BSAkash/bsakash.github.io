cf4950e303a73bac02f0906ecd334cad6a747618
BSAkash <73155813+BSAkash@users.noreply.github.com>
2025-02-25 12:13:56 +0000

    Deploying to gh-pages from @ BSAkash/bsakash.github.io@2749355cecf3b5aa63287ff891f69576a4ada2ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,9 +75,9 @@ ninja.data = [{
           description: "Enhanced nanoGPT architecture with optimized key/query vector sizes, sliding window attention, register tokens, advanced MLP layers, and alternative Softmax approximations, improving efficiency and performance.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
